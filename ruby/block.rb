36c121e6f11bf9a62e2f29cfe9477703276b0924
activesys <activesys.wb@gmail.com>
2014-02-27 22:46:57 +0800

    ruby methods.

--- ruby/block.rb
+++ ruby/block.rb
@@ -11,3 +11,11 @@ eg = ProcExample.new
 eg.pass_in_block {|param| puts "The parameter is #{param}"}
 eg.use_proc 99
 
+my_proc = Proc.new {|param| puts "#{param} in Proc"}
+eg.pass_in_block(&my_proc)
+eg.use_proc(100)
+
+my_lambda = -> param {puts "#{param} in lambda"}
+eg.pass_in_block(&my_lambda)
+eg.use_proc(200)
+
