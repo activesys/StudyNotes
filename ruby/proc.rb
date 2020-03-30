68f3669f17778208929d0fa5b6a5fda9e26d6b20
wangbo <wangbo@gyee-china.com>
2020-03-30 22:56:00 +0800

    Update docs.

--- ruby/proc.rb
+++
@@ -1,8 +0,0 @@
-p1 = Proc.new {|param| puts "You called me with #{param}"}
-p2 = lambda {|param| puts "You called me with #{param}"}
-p3 = -> param {puts "You called me with #{param}"}
-
-p1.call 111
-p2.call 99
-p3.call 88
-
